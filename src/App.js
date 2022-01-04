c3f7ac309eb9372234e9a04007cb224d39a1e5c0
Gabriel Naoto <gabriel@constructor.io>
2022-01-04 00:11:29 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,159 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import {
+  Box, Button, Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  VStack,
+  StackDivider,
+} from '@chakra-ui/react'
 
 function App() {
+  const [running, setRunning] = useState(false);
+  // const [workers, setWorkers] = useState({ ...[...Array(50).keys()].map((index) => ({ [String(index)]: false })) });
+
+
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const startGame = () => {
+    setRunning(true);
+  };
+
+  useEffect(() => {
+    if (running) {
+      setTimeout(() => {
+        setRunning(false);
+      }, 20 * 1000);
+    }
+  }, [running]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Box w='800px' h='800px' bg='white' p={4}>
+        {
+          running
+            ? <Button colorScheme='red' onClick={onOpen}>Menu</Button>
+            : <Button colorScheme='blue' onClick={startGame}>Start</Button>
+        }
+      </Box>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent maxHeight={400} overflow="auto">
+          <ModalHeader>Selecione os trabalhadores</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <VStack
+              divider={<StackDivider borderColor='gray.200' />}
+              spacing={4}
+              align='stretch'
+            >
+              <Box h='40px' justifyContent="space-between" flexDirection="row" display="flex">
+                <Button colorScheme='green' mr={3} onClick={() => console.log('working')}>
+                  Work
+                </Button>
+              </Box>
+              {/* <Box h='40px' justifyContent="space-between" flexDirection="row" display="flex">
+                <Button colorScheme='green' mr={3} >
+                  Work
+                </Button>
+              </Box>
+              <Box h='40px' justifyContent="space-between" flexDirection="row" display="flex">
+                <Button colorScheme='green' mr={3} >
+                  Work
+                </Button>
+              </Box>
+              <Box h='40px' justifyContent="space-between" flexDirection="row" display="flex">
+                <Button colorScheme='green' mr={3} >
+                  Work
+                </Button>
+              </Box>
+              <Box h='40px' justifyContent="space-between" flexDirection="row" display="flex">
+                <Button colorScheme='green' mr={3} >
+                  Work
+                </Button>
+              </Box>
+              <Box h='40px' justifyContent="space-between" flexDirection="row" display="flex">
+                <Button colorScheme='green' mr={3} >
+                  Work
+                </Button>
+              </Box>
+              <Box h='40px' justifyContent="space-between" flexDirection="row" display="flex">
+                <Button colorScheme='green' mr={3} >
+                  Work
+                </Button>
+              </Box>
+              <Box h='40px' justifyContent="space-between" flexDirection="row" display="flex">
+                <Button colorScheme='green' mr={3} >
+                  Work
+                </Button>
+              </Box>
+              <Box h='40px' justifyContent="space-between" flexDirection="row" display="flex">
+                <Button colorScheme='green' mr={3} >
+                  Work
+                </Button>
+              </Box>
+              <Box h='40px' justifyContent="space-between" flexDirection="row" display="flex">
+                <Button colorScheme='green' mr={3} >
+                  Work
+                </Button>
+              </Box>
+              <Box h='40px' justifyContent="space-between" flexDirection="row" display="flex">
+                <Button colorScheme='green' mr={3} >
+                  Work
+                </Button>
+              </Box>
+              <Box h='40px' justifyContent="space-between" flexDirection="row" display="flex">
+                <Button colorScheme='green' mr={3} >
+                  Work
+                </Button>
+              </Box>
+              <Box h='40px' justifyContent="space-between" flexDirection="row" display="flex">
+                <Button colorScheme='green' mr={3} >
+                  Work
+                </Button>
+              </Box>
+              <Box h='40px' justifyContent="space-between" flexDirection="row" display="flex">
+                <Button colorScheme='green' mr={3} >
+                  Work
+                </Button>
+              </Box>
+              <Box h='40px' justifyContent="space-between" flexDirection="row" display="flex">
+                <Button colorScheme='green' mr={3} >
+                  Work
+                </Button>
+              </Box> */}
+
+              {/* {[Object.keys(workers)].map((key) => (
+                <Box h='40px' justifyContent="space-between" flexDirection="row" display="flex">
+                  {key}
+
+                  <Button colorScheme='green' mr={3} disabled={workers[key]} onClick={() => {
+                    setWorkers({
+                      ...workers,
+                      [key]: true,
+                    })
+                  }}>
+                    {workers[key] ? 'Working...' : 'Work'}
+                  </Button>
+                </Box>
+              ))} */}
+
+            </VStack>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button variant='ghost' mr={3} onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal></>
   );
 }
 
